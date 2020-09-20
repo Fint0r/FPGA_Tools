ecad8a13c87672f2c7f50f425012dd2d2323843d
Fintor Jozsef <fintor976@gmail.com>
2020-09-20 15:17:07 +0200

    Corrected exe generation issues.

--- misc/generate_exe.bat
+++ misc/generate_exe.bat
@@ -1,10 +1,11 @@
 REM removing generated files before new generation
 rmdir /s /q build
 rmdir /s /q dist
-del /s /q "FPGA_Tools.spec"
+del /s /q FPGA_Tools.spec
+del /s /q "..\runnable exe\FPGA_Tools.exe"
 
 REM generating new exe
-pyinstaller --onefile --noconsole --name FPGA_Tools ..\src\qqq.py
+pyinstaller --onefile --noconsole ..\src\FPGA_Tools.py
 
 REM copying generated exe to runnable exe folder
 copy "dist\FPGA_Tools.exe" "..\Runnable exe\FPGA_Tools.exe"
